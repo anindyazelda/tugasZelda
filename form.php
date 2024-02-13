5afae6c0ce2b58575986c67bb41553ff732c8eb9
anindyazelda <159877524+anindyazelda@users.noreply.github.com>
2024-02-13 14:24:49 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') or die("No Access Allowed");?>
+
+<h1 class="uk-heading-medium uk-margin-remove-top uk-letter-spacing-xl">Pemilihan Ketua Osis 2024/2025</h1>
+								<p class="uk-text-lead uk-width-4-5@m">
+									SMK Negeri 6 Malang</p><div class="row">
+    <div class="col-md-4 col-md-offset-1">
+
+        </div>
+    </div>
+    <div class="col-md-6 form">
+        <span class="info-login">Silahkan masukkan username untuk dapat melakukan pemilihan</span>
+        <br />
+        <br />
+        <form action="" method="post">
+    <div class="uk-width-1-1 uk-first-column">
+      <input class="uk-input uk-form-large" type="number" name="nis" placeholder="Masukkan username" maxlength="" width="20%" required>
+    </div>
+    
+         
+            <br />
+            <div class="row">
+      <button type="submit" name="submit" class="uk-button uk-button-large uk-button-primary" style="border-radius: 45px;">Submit</button>
+    </div>
+            </div>
+        </form>
+    </div>
+</div>
